c9e8ae71f3f30d3ad457c363c5a7715afabd29ac
Michael Gentile <mgentile90@live.com>
2015-11-06 16:49:55 -0500

    Instructions and Clear Input
    Added instructions for the app and added and automatic clear once a item
    is a added to the list after button click.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ $( document ).ready(function() {
   	$(".add-btn").on("click",function(){
   		var text = $(".input").val();
   		var li = "<li>" + text + "</li>";
-  		$(".item").append(li); 
+  		$(".item").append(li);
+      $(".input").val(""); 
   	});
   	$("li").on("dblclick","li", function() {
   		$(this).remove();
